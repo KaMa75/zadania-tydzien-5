c3205b233dc79a0b45275ff66f4675501f4adf78
KaMa75 <karolmarszalek@o2.pl>
2020-11-19 18:20:02 +0100

    install classnames

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Button.scss';
 
-function Button({children}) {
+function Button({children, outlined}) {
     return (
         <button className={}>
             {children}
